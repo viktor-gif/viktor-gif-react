74baf13498691101e1457a99251fcb56f833a576
viktor <klim-victor.ua@gmail.com>
2021-01-11 17:02:32 +0200

    Updates

---
+++ precache-manifest.ff463f9ff39bc9d323c554dcb0e35bd0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2640191145579229cc998bca59491dfa",
+    "url": "/viktor-gif-react/index.html"
+  },
+  {
+    "revision": "2cc4a3f8f3f283ebd3e1",
+    "url": "/viktor-gif-react/static/css/3.8be3a84e.chunk.css"
+  },
+  {
+    "revision": "2aeaceb9d38ee031458a",
+    "url": "/viktor-gif-react/static/css/main.134e72db.chunk.css"
+  },
+  {
+    "revision": "b705ebbd908803e917b4",
+    "url": "/viktor-gif-react/static/js/2.00844285.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/viktor-gif-react/static/js/2.00844285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc4a3f8f3f283ebd3e1",
+    "url": "/viktor-gif-react/static/js/3.4fe0b4d9.chunk.js"
+  },
+  {
+    "revision": "2aeaceb9d38ee031458a",
+    "url": "/viktor-gif-react/static/js/main.5792cfcb.chunk.js"
+  },
+  {
+    "revision": "f43dfd4a7d8b52cc85d3",
+    "url": "/viktor-gif-react/static/js/runtime-main.de99a2d1.js"
+  },
+  {
+    "revision": "29be27c11136823fe0bc7194ab5e102b",
+    "url": "/viktor-gif-react/static/media/821ea91e3d8c.29be27c1.mp3"
+  },
+  {
+    "revision": "70464abccf777b3e8327096dde8518e2",
+    "url": "/viktor-gif-react/static/media/classic.70464abc.mp3"
+  },
+  {
+    "revision": "890fcfca47dac2fe8184150d1bc04ace",
+    "url": "/viktor-gif-react/static/media/dart.890fcfca.png"
+  },
+  {
+    "revision": "16c8c0bd2f26ee2734dc49ee6c8ccb4e",
+    "url": "/viktor-gif-react/static/media/warriors.16c8c0bd.mp3"
+  },
+  {
+    "revision": "36d70d8e782115b903be07de44a302b6",
+    "url": "/viktor-gif-react/static/media/wedding.36d70d8e.jpg"
+  },
+  {
+    "revision": "11a7654868bd496ad0e50cc429419d38",
+    "url": "/viktor-gif-react/static/media/yoda.11a76548.jpeg"
+  }
+]);
